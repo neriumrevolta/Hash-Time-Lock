d995eeff76d2ea94dd22c637b9d6f42ebd8296c2
arthas168 <pavlovskipetko@abv.bg>
2019-11-15 14:28:56 +0200

    tested more functionalities

--- index.js
+++ index.js
@@ -17,29 +17,42 @@ const main = async () => {
   const contract = await client.getContractInstance(contractSource);
   await contract.deploy();
 
-//   const result = await contract.call(
-//     "dummy_func",
-//     [
-//       "000102030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0f",
-//       "000202030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0f"
-//     ],
-//     { amount: 1000000 }
-//   );
-//   console.log(result.decodedResult);
+  const newContract1 = await contract.call(
+    "new_contract",
+    [
+      "5939679548",
+      "1773653015196",
+      "000102030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0f",
+      "ak_2mwRmUeYmfuW93ti9HMSUJzCk1EYcQEfikVSzgo6k2VghsWhgU",
+      "TRX",
+      "0x9cc7a534cf742cdb9ee16fbf6b5f48a09e485c52"
+    ],
+    { amount: 1000000 }
+  );
 
-    const result = await contract.call(
-      "new_contract",
-      [
-        "5939679548",
-        "1773653015196",
-        "000102030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0f",
-        "ak_2mwRmUeYmfuW93ti9HMSUJzCk1EYcQEfikVSzgo6k2VghsWhgU",
-        "TRX",
-        "0x9cc7a534cf742cdb9ee16fbf6b5f48a09e485c52"
-      ],
-      { amount: 1000000 }
-    );
-    console.log(result.decodedResult);
+  console.log(newContract1.decodedResult.id);
+
+  const newContract2 = await contract.call(
+    "new_contract",
+    [
+      "6939679548",
+      "1873653015196",
+      "000202030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0f",
+      "ak_2mwRmUeYmfuW93ti9HMSUJzCk1EYcQEfikVSzgo6k2VghsWhgU",
+      "TRX",
+      "0x9gc7a534cf742cdb9ee16fbf6b5f48a09e485c52"
+    ],
+    { amount: 1000000 }
+  );
+
+  console.log(newContract2.decodedResult.id);
+
+  const getOneStatus = await contract.call("get_many_status", [
+    ["494b7aebb005f1724600b49544771f9d573d259a3b29d01b878e26287182695f",
+    "9a891b000cc6385398aa1cbd9e80c867965af00d32400e88ce08477660cb59ab"
+  ]]);
+
+  console.log(getOneStatus.decodedResult);
 };
 
 main();
